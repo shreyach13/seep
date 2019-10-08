e70e9a820b735a7d809a57b72b2949689e93cd95
Shreya.Chatterjee <shreya.chatterjee@ShreyaChatterjees-MacBook-Pro.local>
2019-10-08 15:45:16 +0200

    eks vpc commit

--- eks/deploy/app/app_main_eks.tf
+++ eks/deploy/app/app_main_eks.tf
@@ -5,3 +5,13 @@ provider "aws" {
 module "seep-sg-eks" {
   source = "../module/sg/"
 }
+
+module "seep-vpc-eks" {
+  source = "../module/vpc/"
+}
+
+module "seep-eks-eks" {
+  source = "../module/eks-cluster/"
+  mike_is_awesome = [module.seep-vpc-eks.seep_first_subnet, module.seep-vpc-eks.seep_second_subnet]
+  default-vpc = module.seep-vpc-eks.vpc.id
+  }
