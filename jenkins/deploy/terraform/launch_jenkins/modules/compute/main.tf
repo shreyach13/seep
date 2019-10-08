179d13e977b50f02c4cc83cbbd3437df73102ebd
Eric.Echter <eric.echter@EricEchters-MacBook-Pro.local>
2019-10-08 10:36:52 +0200

    Added new tools to Jenkins AMI

--- jenkins/deploy/terraform/launch_jenkins/modules/compute/main.tf
+++ jenkins/deploy/terraform/launch_jenkins/modules/compute/main.tf
@@ -9,21 +9,22 @@ data "aws_ami" "seep-ami-jenkins" {
   owners = [var.aws_account_owner]
 }
 
-/*resource "aws_key_pair" "eric-key" {
+resource "aws_key_pair" "eric-key" {
   key_name   = "eric-key"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDCrXmHHrI+9PR4m7/8imIRrwQWF6md+1oaCRZM/r8jpfhLqqsN8pIUztl+Lu6PxGp9KdJFIlKxUhcMn1pINXYdETq6Tp0lIUjxXLrA9eUkdjt6TU7UG3vR1ibR4Nx2SGwULntsrUNDoYQX8l1AGgM4jLLQoba45xIFfVRlUkvcHaReifHsDQHnYsHBSZ0tg0GI9KUnPBgyoNa9/eBgPLH3NbQdg98lKdjqSdRNITT2qGe6GyS1On2OWJiOYqNwhQQY8zwAaPhDpcthKH+Vp7+sB3odL3CQzAhZnOie4+6xsV/uIPkljYrrJDGfqGonekUpuWR9lSjPTe9ZxY6N4bVz eric.echter@EricEchters-MacBook-Pro.local"
-}*/
+}
 
 resource "aws_instance" "webserver" {
   ami           = "${data.aws_ami.seep-ami-jenkins.id}"
   instance_type = "t2.micro"
   /*iam_instance_profile = var.iam_role*/
-  /*key_name = "eric-key"*/
+  key_name = "eric-key"
   security_groups = [
     var.my_security_group
   ]
   tags = {
-    Name  = "seep-jenkins"
-    owner = var.owner
+    Name        = "seep-jenkins-production"
+    owner       = var.owner
+    environment = "production"
   }
 }
--- jenkins/deploy/terraform/launch_jenkins/modules/security/main.tf
+++ jenkins/deploy/terraform/launch_jenkins/modules/security/main.tf
@@ -2,23 +2,24 @@ provider "aws" {
   region = "eu-west-1"
 }
 
-resource "aws_security_group" "seep-sg-jenkins" {
-  name = "seep-sg-jenkins"
+resource "aws_security_group" "seep-sg-jenkins-production" {
+  name = "seep-sg-jenkins-production"
   tags = {
-    Name  = "seep-sg-jenkins"
-    owner = var.owner
+    Name        = "seep-sg-jenkins-production"
+    owner       = var.owner
+    environment = "production"
   }
   ingress {
     from_port   = var.from_port
     to_port     = var.to_port
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["185.46.212.0/22"]
   }
   ingress {
     from_port   = "22"
     to_port     = "22"
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["185.46.212.0/22"]
   }
   egress {
     from_port   = 0
