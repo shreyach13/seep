f4de36862dbc46f28bc0cd38c038962712c22b4a
Shreya.Chatterjee <shreya.chatterjee@shreyacrjeesmbp.clients.europe.shell.com>
2019-10-09 17:22:03 +0200

    added python code

--- jenkins/config-testing/test_tags.py
+++ jenkins/config-testing/test_tags.py
@@ -7,9 +7,9 @@ datastore = ''
 
 print (os.getcwd())
 for filename in os.listdir(os.getcwd()):
-    if filename.endswith(".json"): 
+    if filename.endswith(".json"):
         out_file = os.path.join(os.getcwd(),filename)
         print (out_file)
         with open(out_file, 'r') as f:
             datastore = json.load(f)
-            print (datastore)
+            return (datastore)
